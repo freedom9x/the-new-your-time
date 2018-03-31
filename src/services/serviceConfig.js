9066b19d4c852f576c304818bce3ea8226a2a84a
hien pham <hien.pham@onsolve.com>
2018-03-31 18:00:43 +0700

    use https

--- src/services/serviceConfig.js
+++ src/services/serviceConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiKey = '13ab15b049e548bf9d4410f8ce326004'
-const baseURL = 'http://api.nytimes.com/'
+const baseURL = 'https://api.nytimes.com/'
 export const service = axios.create({
   baseURL,
 })
